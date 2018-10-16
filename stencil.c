e15f08176684f8f9a06c3f6ec12606270a679dae
Alex Stedman <alexstedman@eduroam-161-104.nomadic.bris.ac.uk>
2018-10-16 14:18:51 +0100

    changes to stencil calculations

--- stencil.c
+++ stencil.c
@@ -52,11 +52,11 @@ int main(int argc, char *argv[]) {
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
   for (int j = 0; j < ny; ++j) {
     for (int i = 0; i < nx; ++i) {
-      tmp_image[j+i*ny] = image[j+i*ny] * 3.0/5.0;
-      if (i > 0)    tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.5/5.0;
-      if (i < nx-1) tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.5/5.0;
-      if (j > 0)    tmp_image[j+i*ny] += image[j-1+i*ny] * 0.5/5.0;
-      if (j < ny-1) tmp_image[j+i*ny] += image[j+1+i*ny] * 0.5/5.0;
+      tmp_image[j+i*ny] = image[j+i*ny] * 0.6;
+      if (i > 0)    tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.1;
+      if (i < nx-1) tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
+      if (j > 0)    tmp_image[j+i*ny] += image[j-1+i*ny] * 0.1;
+      if (j < ny-1) tmp_image[j+i*ny] += image[j+1+i*ny] * 0.1;
     }
   }
 }
